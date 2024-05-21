bba9e5d555ec5acb890a1613d1cd18cce4e7793c
MannantaB <mannanta18@gmail.com>
2024-05-21 15:58:54 +0700

    add metode tambah anak untuk kelas ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -12,4 +12,9 @@ public:
 	~ibu() {
 		cout << "Ibu \"" << nama << "\" tidak ada\n";
 	}
+	void tambahAnak(anak*);
+	void cetakAnak();
 };
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
